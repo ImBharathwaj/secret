05f6a08fa25464f0f926b2c894bddab860856b5b
ImBharathwaj <bharathwajist@gmail.com>
2025-01-19 10:19:29 +0530

    Updated

---
+++ templates/rest_api_template.py
@@ -0,0 +1,28 @@
+import dlt
+from dlt.sources.rest_api import rest_api_source
+
+github_client = RESTClient(base_url="https://api.github.com")  # (1)
+
+@dlt.resource
+def get_issues():
+    for page in github_client.paginate(                        # (2)
+        "/repos/Datavault-UK/automate-dv/issues",                           # (3)
+        params={                                               # (4)
+            "per_page": 100,
+            "sort": "updated",
+            "direction": "desc",
+        },
+    ):
+        yield page                                             # (5)
+
+
+pipeline = dlt.pipeline(
+    pipeline_name="rest_api_pipeline",
+    destination="postgres",
+    dataset_name="rest_api_data",
+)
+
+load_info = pipeline.run(get_issues)
+
+load_info = pipeline.run(get_issues)
+print(load_info)
